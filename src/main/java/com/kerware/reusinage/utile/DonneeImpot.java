c90fd7affbae7ff4e66d11617f115aea61419b73
lucas_demaimay <demaimay.lucas@gmail.com>
2025-05-02 18:55:33 +0200

    Separation des donnees pour impot

---
+++ src/main/java/com/kerware/reusinage/utile/DonneeImpot.java
@@ -0,0 +1,80 @@
+package com.kerware.reusinage.utile;
+
+import com.kerware.reusinage.parametres.SituationFamiliale;
+
+public class DonneeImpot {
+
+    private int revenuNetDeclarant1;
+    private int revenuNetDeclarant2;
+
+    private int nbEnfants;
+    private int nbEnfantsHandicapes;
+
+    private SituationFamiliale situationFamiliale;
+
+    private boolean parentIsole;
+
+    public DonneeImpot(int revenuNetDeclarant1, int revenuNetDeclarant2, int nbEnfants, int nbEnfantsHandicapes,
+            SituationFamiliale situationFamiliale, boolean parentIsole) {
+
+        if(revenuNetDeclarant1 < 0 || revenuNetDeclarant2 < 0)
+            throw new IllegalArgumentException("Le revenu net ne peut pas être négatif");
+        
+        if(nbEnfants < 0)
+            throw new IllegalArgumentException("Le nombre d'enfants ne peut pas être négatif");
+        
+        if(nbEnfantsHandicapes < 0)
+            throw new IllegalArgumentException("Le nombre d'enfants handicapés ne peut pas être négatif");
+        
+        if(situationFamiliale == null)
+            throw new IllegalArgumentException("La situation familiale ne peut pas être null");
+        
+        if(nbEnfantsHandicapes > nbEnfants)
+            throw new IllegalArgumentException("Le nombre d'enfants handicapés ne peut pas être supérieur au nombre d'enfants");
+        
+        if(nbEnfants > 7)
+            throw new IllegalArgumentException("Le nombre d'enfants ne peut pas être supérieur à 7");
+        
+        boolean seul = situationFamiliale == SituationFamiliale.CELIBATAIRE || situationFamiliale == SituationFamiliale.DIVORCE || situationFamiliale == SituationFamiliale.VEUF;
+        
+        if(parentIsole && !seul)
+            throw new IllegalArgumentException("Un parent isolé ne peut pas être marié ou pacsé");
+        
+        if(revenuNetDeclarant2 > 0 && seul)
+            throw new IllegalArgumentException("Un célibataire, un divorcé ou un veuf ne peut pas avoir de revenu pour le déclarant 2");
+        
+        this.revenuNetDeclarant1 = revenuNetDeclarant1;
+        this.revenuNetDeclarant2 = revenuNetDeclarant2;
+        this.nbEnfants = nbEnfants;
+        this.nbEnfantsHandicapes = nbEnfantsHandicapes;
+        this.situationFamiliale = situationFamiliale;
+        this.parentIsole = parentIsole;
+    }
+
+    public int getRevenuNetDeclarant1() {
+        return revenuNetDeclarant1;
+    }
+
+    public int getRevenuNetDeclarant2() {
+        return revenuNetDeclarant2;
+    }
+
+    public int getNbEnfants() {
+        return nbEnfants;
+    }
+
+    public int getNbEnfantsHandicapes() {
+        return nbEnfantsHandicapes;
+    }
+
+    public SituationFamiliale getSituationFamiliale() {
+        return situationFamiliale;
+    }
+
+    public boolean isParentIsole() {
+        return parentIsole;
+    }
+
+    
+
+}
